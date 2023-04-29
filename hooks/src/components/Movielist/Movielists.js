b73fc21a974e2e943f01f019489be16972b33ee1
faraowait <arafabubakar4@gmail.com>
2023-04-29 19:18:42 +0100

    hooks

---
+++ hooks/src/components/Movielist/Movielists.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MovieList = ({ movies }) => {
+  return (
+    <div className="movie-list">
+      {movies.map(movie => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </div>
+  );
+};
+
+export default MovieList;
